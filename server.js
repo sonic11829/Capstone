1c96f3ca3fd7975f28d7ea2ee3f616cd7d42b095
sonic11829 <sethneal15@ymail.com>
2022-09-30 21:50:51 -0400

    made a tip calculator

--- server.js
+++ server.js
@@ -13,10 +13,31 @@ app.get('/greeting/:name', (req, res) => {
   res.send('Hey there ' + name + '!')
 })
 
-// app.get('/fruits/:i', (req, res) => {
-//   const i = req.params.i
-//   res.send(fruits[i])
-// })
+//Tip Calculator
+//Your app should have a route of '/tip'and it should expect 2 params. 
+//One should be total and one should be tipPercentage.
+//When hitting the route, the page should display how much your tip will -
+//- be based on the total amount of the bill and the tip percentage. 
+//(ex. hitting '/tip/100/20'should display 20 on the page).
+
+const tip = [{
+  cost: parseInt()
+}]
+
+app.get('/tip', (req, res) => {
+  let cost = req.query.cost;
+  let percentage = req.query.percentage;
+  let tip = cost * (percentage/100)
+  let resString = `cost is ${cost}, percentage is ${percentage}, tip is ${tip}`
+  res.send(resString)
+})
+
+
+
+
+
+
+
 // Tell the app to listen on port 3000
 // for HTTP requests from clients
 app.listen(3000, function () {
